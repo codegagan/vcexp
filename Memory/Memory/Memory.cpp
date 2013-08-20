93a932affd27301afdc1360b96b8d44d04f62966
Gagandeep Singh <codegagan@gmail.com>
2013-08-20 20:27:47 +0530

    VC++ 2010 Express project; first time commit

---
+++ Memory/Memory/Memory.cpp
@@ -0,0 +1,23 @@
+// Memory.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	void * p=NULL;
+	long i=0;
+	p=malloc(1);
+	cout<<"Check you memory (RAM) usage and press enter to start";
+	getchar();
+	cout<<"Lemme see, how much memory of your system I can take...."<<endl;
+	while(p){
+	p=malloc(100000);
+	i++;
+	}
+	cout<<"Got bytes (X 100000) :"<<i<<endl;
+	cout<<"Check you memory (RAM) usage now";
+	getchar();
+	return 0;
+}
