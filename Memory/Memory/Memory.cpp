2bb89fcb284e8210ae6ff19b359f8da9a34bff00
Gagandeep Singh <codegagan@gmail.com>
2013-08-25 17:26:18 +0530

    modified that release version also retains memory

--- Memory/Memory/Memory.cpp
+++ Memory/Memory/Memory.cpp
@@ -7,14 +7,19 @@ using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 {
 	void * p=NULL;
+	void * pa[30000];
 	long i=0;
 	p=malloc(1);
 	cout<<"Check you memory (RAM) usage and press enter to start";
 	getchar();
 	cout<<"Lemme see, how much memory of your system I can take...."<<endl;
 	while(p){
+	
 	p=malloc(100000);
+	if(p)memset(p, 1,100000);
+	pa[i]=p;
 	i++;
+	if(i>30000)break;
 	}
 	cout<<"Got bytes (X 100000) :"<<i<<endl;
 	cout<<"Check you memory (RAM) usage now";
